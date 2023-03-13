57044570f123bc85f4cf7a245569712cb78c068e
Rafal13 <rafal.karczewski@gmail.com>
2023-03-13 08:15:32 +0100

    Grid1

---
+++ src/GridComponent/GridComponent.tsx
@@ -0,0 +1,19 @@
+import './GridComponent.scss'
+
+const GridComponent = () => {
+   return (
+      <div className='gridComponent-container'>
+  <div className="grid-item1">1</div>
+  <div className="grid-item2">2</div>
+  <div className="grid-item3">3</div>  
+  <div className="grid-item4">4</div>
+  <div className="grid-item5">5</div>
+  <div className="grid-item6">6</div>  
+  <div className="grid-item7">7</div>
+  <div className="grid-item8">8</div>
+  {/* <div className="grid-item">9</div> */}
+      </div>
+   );
+};
+
+export default GridComponent;
